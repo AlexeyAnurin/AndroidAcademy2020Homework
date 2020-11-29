ac8f393e6f8d89ef43efb62178c0491ffaa13f51
Alex Anurin <Anurin.Alexey@gmail.com>
2020-11-29 18:53:29 +0200

    Initial commit

---
+++ app/src/main/java/ru/skillbranch/androidacademy2020homework2/MovieDetailsActivity.kt
@@ -0,0 +1,13 @@
+package ru.skillbranch.androidacademy2020homework2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MovieDetailsActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+
+}
